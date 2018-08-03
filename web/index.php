acc4f025d633da9c9b07d8f32777fd58a0fb0c60
Karthik <karthik21@live.in>
2018-08-03 21:09:25 +0530

    load all file details

--- web/index.php
+++ web/index.php
@@ -226,21 +226,35 @@ iframe {
        * Print files.
        */
       function listFiles() {
-        gapi.client.drive.files.list({
-          'pageSize': 10,
-          'fields': "nextPageToken, files(id, name)"
-        }).then(function(response) {
-          appendPre('Files:');
-          var files = response.result.files;
-          if (files && files.length > 0) {
-            for (var i = 0; i < files.length; i++) {
-              var file = files[i];
-              appendPre(file.name + ' (' + file.id + ')');
-            }
-          } else {
-            appendPre('No files found.');
-          }
-        });
+          var callback = loadAllFiles;
+    	  var getPageOfFiles = function(request, result) {
+    			request.execute(function(resp) {
+    				//result = resp.messages;
+    				resp.files = resp.files || [];
+    				result.concat(resp.files);
+    				var nextPageToken = resp.nextPageToken;
+    				if (nextPageToken) {
+    					request = gapi.client.drive.files.list({
+    				          'pageSize': 1000,
+    				          'pageToken': nextPageToken,
+    				          'fields': "nextPageToken, files(id, name)"
+    				        });
+    					getPageOfFiles(request, result);
+    				} else {
+    					callback(result);
+    				}
+    			});
+    		};
+    		var initialRequest = gapi.client.drive.files.list({
+    	          'pageSize': 1000,
+    	          'fields': "nextPageToken, files(id, name)"
+    	        });
+    		getPageOfFiles(initialRequest, []);
+      }
+      function loadAllFiles(files){
+		for(int i=0; i < files.length; i++){
+			appendPre(files[i]);
+		}
       }
 
     </script>
