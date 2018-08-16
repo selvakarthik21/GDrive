97dee29a40625f8fabba6dab986d690343b73bda
Karthik <karthik21@live.in>
2018-08-16 23:18:04 +0530

    reverted to Bootstrap version

--- web/index.php
+++ web/index.php
@@ -4,10 +4,9 @@
 <title>Action Items Manager</title>
 <meta charset="UTF-8">
 
-<link rel="stylesheet" href="css/material.min.css">
-<link rel="stylesheet" href="css/icon.css">
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="css/bootstrap-theme.min.css">
 <link rel="stylesheet" href="css/jquery.dataTables.min.css">
-<link rel="stylesheet" href="css/dataTables.material.min.css">
 
 <style>
 iframe {
@@ -49,8 +48,8 @@ td.reorder{
 }
 .logo{
 	background: url(img/logo.png) no-repeat;
-    width: 150px;
-    height: 56px;
+    width: 100%;
+    height: 60px;
     background-repeat: no-repeat;
     background-size: auto 100%;
 }
@@ -60,47 +59,69 @@ td.reorder{
 .container{
 	margin-top: 10px;
 }
-
 </style>
 </head>
-<body class="mdl-demo mdl-color--grey-100 mdl-color-text--grey-700 mdl-base">
-	<div class="mdl-layout__container has-scrolling-header">
-		<div
-			class="mdl-layout mdl-js-layout mdl-layout--fixed-header has-tabs is-upgraded"
-			data-upgraded=",MaterialLayout">
-			<header
-				class="mdl-layout__header mdl-layout__header--scroll mdl-color--primary">
+<body>
+	<div class="container">
+		<div class="logo"></div>		
+		<div class="title">Action Items Manager</div>
 
-				<div class="mdl-layout--large-screen-only mdl-layout__header-row">
-					<div class="logo"></div> <h3>Action Items Manager</h3>
-				</div>
-
-
-			</header>
-			<main class="mdl-layout__content">
-
-			<button id="authorize_button" class="btn btn-primary hidden"
-				style="margin-top: 50px;">Authorize</button>
-
-			<table class="mdl-data-table mdl-js-data-table mdl-data-table--selectable mdl-shadow--2dp table-inbox hidden dataTable">
-				<thead>
-					<tr>
-						<th style="width: 0%;"></th>
-						<th style="width: 10%;">Name</th>
-						<th style="width: 12%;">Created On</th>
-						<th style="width: 53%">Action Item</th>
-						<th style="width: 25%; padding-right: 0px;"></th>
-					</tr>
-				</thead>
-				<tbody></tbody>
-			</table>
-			<div class="overlay">
-				<div id="loading-img"></div>
-			</div>
-			</main>
+		<button id="authorize_button" class="btn btn-primary hidden" style="margin-top: 50px;">Authorize</button>
+		
+		<table class="table table-striped table-inbox hidden dataTable">
+			<thead>
+				<tr>
+					<th style="width: 0%;"></th>
+					<th style="width: 10%;">Document</th>
+					<th style="width: 10%;">Created At</th>
+					<th style="width: 57%">Action Item Message</th>
+					<th style="width: 23%"></th>
+				</tr>
+			</thead>
+			<tbody></tbody>
+		</table>
+		<div class="overlay">
+		    <div id="loading-img"></div>
 		</div>
 	</div>
-	<div class="modal fade" id="reply-modal" tabindex="-1" role="dialog" style="display:none;">
+
+	<div class="modal fade" id="compose-modal" tabindex="-1" role="dialog">
+		<div class="modal-dialog modal-lg">
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal"
+						aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button>
+					<h4 class="modal-title">Compose</h4>
+				</div>
+				<form onsubmit="return sendEmail();">
+					<div class="modal-body">
+						<div class="form-group">
+							<input type="email" class="form-control" id="compose-to"
+								placeholder="To" required />
+						</div>
+
+						<div class="form-group">
+							<input type="text" class="form-control" id="compose-subject"
+								placeholder="Subject" required />
+						</div>
+
+						<div class="form-group">
+							<textarea class="form-control" id="compose-message"
+								placeholder="Message" rows="10" required></textarea>
+						</div>
+					</div>
+					<div class="modal-footer">
+						<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+						<button type="submit" id="send-button" class="btn btn-primary">Send</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+
+	<div class="modal fade" id="reply-modal" tabindex="-1" role="dialog">
 		<div class="modal-dialog modal-lg">
 			<div class="modal-content">
 				<div class="modal-header">
@@ -138,9 +159,9 @@ td.reorder{
 	</div>
 
 	<script src="js/jquery-2.2.4.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
 	<script src="js/jquery.dataTables.min.js"></script>
 	<script src="js/dataTables.rowReorder.min.js"></script>
-	<script src="js/material.min.js"></script>
 	<script src="js/dataTables.material.min.js"></script>
 	<script src="js/options.js"></script>
 	
